ad53a638d262e137ed78fd6b32331a71baefb169
avigail-hasida-healthy <avigail.hasida@healthy.io>
2023-09-26 13:15:53 +0300

    feat: Update helmFiles to .releaserc

--- .releaserc.js
+++ .releaserc.js
@@ -3,7 +3,9 @@ module.exports = {
   enableMilestones: false,
   helmFiles: {
     staging: [
-      'smiling-diode-638/be-staging-uk_adherence-staging.yaml'
+      'smiling-diode-638/be-staging-uk_adherence-staging.yaml',
+      'smiling-diode-638/be-staging-uk_universe-staging.yaml'
+
     ],
     production: [
       'smiling-diode-638/be-production-uk_adherence-staging.yaml'
