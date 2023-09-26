9258b8b4c53c1c05bb037d8e19f4e0a13ff487f3
avigail-hasida-healthy <avigail.hasida@healthy.io>
2023-09-26 11:44:25 +0300

    feat: Add helmFiles to .releaserc

--- .releaserc.js
+++ .releaserc.js
@@ -1,4 +1,12 @@
 module.exports = {
   gcsImageDir: 'tomk-test/test',
-  enableMilestones: true,
+  enableMilestones: false,
+  helmFiles: {
+    staging: [
+      'smiling-diode-638/be-staging-uk_avigail-test-staging.yaml'
+    ],
+    production: [
+      'smiling-diode-638/be-production-uk_avigail-test-staging.yaml'
+    ]
+  }
 };
