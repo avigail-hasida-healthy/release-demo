38987e664ce076d28488e5eca128248651a031eb
avigail-hasida-healthy <avigail.hasida@healthy.io>
2023-09-28 09:09:15 +0300

    feat: Add deploy to .releaserc.js

--- .releaserc.js
+++ .releaserc.js
@@ -2,11 +2,33 @@ module.exports = {
   gcsImageDir: 'tomk-test/test',
   enableMilestones: false,
   helmFiles: {
+    staging: ['smiling-diode-638/be-staging-uk_universe-staging.yaml'],
+    production: ['smiling-diode-638/be-production-uk_universe-staging.yaml']
+  },
+  deploy: {
     staging: [
-      'smiling-diode-638/be-staging-uk_universe-staging.yaml'
-    ],
-    production: [
-      'smiling-diode-638/be-production-uk_universe-staging.yaml'
+      {
+        type: 'jenkins',
+        host: 'https://jenkins-ci.healthy.io',
+        job: 'zz-JenkinsTests/Morad-tests/helm-charts-deployment',
+        parameters: {
+          chart: 'testing-chart'
+        }
+      },
+      {
+        type: 'jenkins',
+        host: 'https://jenkins-ci.healthy.io',
+        job: 'Urine Commercial/Deploy staging',
+        parameters: {
+          labels: 'adherence-develop',
+          helmfiles: 'adherence'
+        }
+      },
+      {
+        type: 'github',
+        workflow: 'deploy.yaml',
+      }
     ]
-  }
+  },
+
 };
