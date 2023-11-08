20ff4cb11e8cb360e2d2836b305ce4b05cf5113c
avigail-hasida-healthy <avigail.hasida@healthy.io>
2023-11-08 12:07:04 +0200

    feat: Restructure releaserc.js

--- .releaserc.js
+++ .releaserc.js
@@ -1,37 +1,33 @@
 module.exports = {
-  gcsImageDir: 'tomk-test/test',
-  enableMilestones: false,
-  helmFiles: {
-    staging: ['smiling-diode-638/be-staging-uk_universe-staging.yaml'],
-    production: ['smiling-diode-638/be-production-uk_universe-staging.yaml']
+  //gcsImageDir: 'tomk-test/test',
+  staging: {
+    // enableMilestones: false,
+    helmFiles: ['smiling-diode-638/be-staging-uk_universe-staging.yaml'],
+    // deploy: [
+    //   {
+    //     type: 'jenkins',
+    //     host: 'https://jenkins-ci.healthy.io',
+    //     job: '/Urine Commercial/Deploy staging',
+    //     parameters: {
+    //       labels: 'name=adherence-cronjobs-develop',
+    //       helmfiles: 'adherence'
+    //     }
+    //   },
+    //   {
+    //     type: 'github',
+    //     workflow: 'deploy.yaml',
+    //   }
+    // ],
+    // k8s: [
+    //   {
+    //     cluster: 'be-staging-uk',
+    //     zone: 'europe-west2-a',
+    //     project: 'smiling-diode-638',
+    //     namespace: 'adherence-staging'
+    //   }
+    // ],
   },
-  deploy: {
-    staging: [
-      {
-        type: 'jenkins',
-        host: 'https://jenkins-ci.healthy.io',
-        job: '/Urine Commercial/Deploy staging',
-        parameters: {
-          labels: 'name=adherence-cronjobs-develop',
-          helmfiles: 'adherence'
-        }
-      },
-      {
-        type: 'github',
-        workflow: 'deploy.yaml',
-      }
-    ],
-    production: []
-  },
-  k8s: {
-    staging: [
-      {
-        cluster: 'be-staging-uk',
-        zone: 'europe-west2-a',
-        project: 'smiling-diode-638',
-        namespace: 'adherence-staging'
-      }
-    ],
-    production: []
+  production: {
+    helmFiles: ['smiling-diode-638/be-production-uk_universe-staging.yaml'],
   }
 };
